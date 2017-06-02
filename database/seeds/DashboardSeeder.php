708cb18533d7f595da0c0d057518c26f03307913
William Llanos <williama.llanos@hotmail.com>
2017-06-01 23:43:53 -0500

    - Fix Default page

--- database/seeds/DashboardSeeder.php
+++ database/seeds/DashboardSeeder.php
@@ -14,7 +14,7 @@ class DashboardSeeder extends Seeder
     {
         $dash = Page::create([
             'name'       => 'Inicio',
-            'content'    => '<h1>Bienvenido al CMS!</h1><p>Esta es una p&aacute;gina creada por defecto, para editarla ve a&nbsp;<a title="Editar P&aacute;gina de Inicio" href="' . url('admin/pages/1/edit') . '">AQU&Iacute;</a>&nbsp;(Pero&nbsp;<a title="Iniciar Sesi&oacute;n" href="' . url('/login') . '">INICIA SESI&Oacute;N</a>&nbsp;primero)</p>',
+            'content'    => '<h1>Bienvenido al CMS!</h1><p>Esta es una p&aacute;gina creada por defecto, para editarla entra&nbsp;<a title="Editar P&aacute;gina de Inicio" href="' . url('admin/pages/1/edit') . '">AQU&Iacute;</a>&nbsp;(Pero&nbsp;<a title="Iniciar Sesi&oacute;n" href="' . url('/login') . '">INICIA SESI&Oacute;N</a>&nbsp;primero)</p>',
             'is_private' => false,
             'position'   => 'none',
             'user_id'    => 1,
